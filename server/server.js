c1b300f8a15b9355ea7e26e811511649da4b54af
JDC <jeanpierre.decuba@student.fontys.nl>
2018-04-21 23:26:07 +0200

    package.json start cmd

--- server/server.js
+++ server/server.js
@@ -2,15 +2,22 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
 
 var http = require('http')
 , https = require('https');
 
 app.use(express.static(publicPath));
 
-//app.listen(80, 443);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
 
-http.createServer(app).listen(80, () => {
-    console.log('test');
-})
-https.createServer(app).listen(443)
+app.listen(port, () => {
+    console.log('wtf');
+});
+
+// http.createServer(app).listen(80, () => {
+//     console.log('test');
+// })
+// https.createServer(app).listen(443)
