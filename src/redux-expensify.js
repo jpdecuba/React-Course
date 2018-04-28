649b62b82d269ba696d9ee7ea306d46d00148a9e
JDC <jeanpierre.decuba@student.fontys.nl>
2018-04-28 19:11:51 +0200

    Working with multiple reducers

--- src/redux-expensify.js
+++ src/redux-expensify.js
@@ -1,5 +1,40 @@
 import {createStore, combineReducers} from 'redux';
 
+const expensesReducerDefaultState = [];
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'data',
+    startDate: undefined,
+    endDate: undefined
+};
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+        return state;
+    }
+}
+
+// Filters reducer
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+        return state;
+    }
+}
+
+// Store creation
+
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer
+    })
+);
+
+console.log(store.getState());
+
 const demoState = {
     expenses: [{
         id: 'sdfsdfs',
