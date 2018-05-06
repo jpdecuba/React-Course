7fddea6d8cfbe33b9ebf7483a24b5c97ffdeb424
JDC <jeanpierre.decuba@student.fontys.nl>
2018-05-06 19:48:03 +0200

    Higher Order Component

---
+++ src/playground/hoc.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Info = (props) => (
+    <div>
+        <h1>Info</h1>
+        <p>the info is {props.info}</p>
+    </div>
+);
+
+const withAdminWarning = (WrappedComponent) => {
+    return (props) => (
+        <div>
+        {props.isAdmin && <p>this is private. please dont share</p>}
+        <WrappedComponent {...props}/>
+        </div>
+    );
+};
+
+const requireAuthentication = (WrappedComponent) => {
+    return (props) => (
+        <div>
+        {props.isAuthenticated ? (<WrappedComponent {...props}/>) :  (<p>please log in</p>)}
+        </div>
+    );
+};
+
+const AdminInfo = withAdminWarning(Info);
+const AuthInfo = requireAuthentication(Info);
+
+// ReactDOM.render(<AdminInfo isAdmin={false} info="what" />, document.getElementById('app'));
+ReactDOM.render(<AuthInfo isAuthenticated={false} info="what" />, document.getElementById('app'));
